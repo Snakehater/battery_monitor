46ce79ec1e049e357372f1b95694023d7e5acd9c
Snakehater <vigor.turujlija@gmail.com>
2022-11-16 13:52:32 +0100

    Added sqlite3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <sqlite3.h>
 
 int main(int argc, char **args)
 {
-	printf("Hello world\n");
+	sqlite3 *db;
+	int rc = sqlite3_open("test.db", &db);
+
+	if (rc)
+	{
+		fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+		return -1;
+	}
+	else
+	{
+		fprintf(stdout, "Opened database successfully\n");
+	}
+
+	sqlite3_close(db);
 	return 0;
 }
